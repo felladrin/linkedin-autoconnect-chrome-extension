61e1e2152be085e99af7519040be2b9a82309aca
Victor Nogueira <felladrin@gmail.com>
2020-10-18 21:33:02 +0300

    Fix selector queries to make it work on latest LinkedIn version

--- src/tab.js
+++ src/tab.js
@@ -1,47 +1,52 @@
-(function() {
+(function () {
   let isAutoConnectRunning = false;
   let autoConnectLastLocation = "";
 
   const linkedInUrl = {
     patternOfSearchPage: "linkedin.com/search/results/people",
-    patternOfMyNetworkPage: "linkedin.com/mynetwork"
+    patternOfMyNetworkPage: "linkedin.com/mynetwork",
   };
   const linkedInSelector = {
-    sendNowButton: "button.ml1.artdeco-button.artdeco-button--3.artdeco-button--primary",
+    sendNowButton: 'button[aria-label="Send now"]',
     cancelButton: 'button[aria-label="Dismiss"] > li-icon[type="cancel-icon"]',
     nextButton: "button.artdeco-pagination__button--next",
-    connectButtonsFromRecommendedPage: 'footer > button[data-control-name="people_connect"]',
-    connectButtonsFromSearchPage: "button.search-result__action-button.search-result__actions--primary:enabled",
+    connectButtonsFromRecommendedPage: "span.artdeco-button__text",
+    connectButtonsFromSearchPage:
+      "button.search-result__action-button.search-result__actions--primary:enabled",
   };
-  
+
   function clickSendNowButtonIfAvailable() {
-    const sendNowButton = document.querySelector(linkedInSelector.sendNowButton);
+    const sendNowButton = document.querySelector(
+      linkedInSelector.sendNowButton
+    );
 
     if (sendNowButton) sendNowButton.click();
-  };
-  
+  }
+
   function dismissEmailRequiredDialog() {
     const cancelButton = document.querySelector(linkedInSelector.cancelButton);
-    
+
     if (cancelButton) cancelButton.click();
   }
-  
+
   function goToNextPage() {
     const nextButton = document.querySelector(linkedInSelector.nextButton);
-  
+
     if (nextButton) nextButton.click();
   }
-  
+
   function addPeopleFromSearchPage() {
     if (!isAutoConnectRunning) return;
 
     let alreadyInvited = 0;
-  
+
     const delayBetweenClicks = 1500;
-    const connectButtons = document.querySelectorAll(linkedInSelector.connectButtonsFromSearchPage);
-  
+    const connectButtons = document.querySelectorAll(
+      linkedInSelector.connectButtonsFromSearchPage
+    );
+
     window.scrollTo(0, document.body.scrollHeight);
-  
+
     if (connectButtons.length > 0) {
       for (const button of connectButtons) {
         setTimeout(() => {
@@ -50,45 +55,57 @@
             clickSendNowButtonIfAvailable();
             button.focus();
             button.click();
+            console.log("%c%s", "color: #731d6d", "!!!!button clicked!!");
             window.scrollBy(0, 200);
-            button.setAttribute("disabled", "true");
-            button["innerText"] = "Invite Sent";
-            dismissEmailRequiredDialog();
             clickSendNowButtonIfAvailable();
+            dismissEmailRequiredDialog();
           }
         }, alreadyInvited * delayBetweenClicks);
 
         alreadyInvited++;
       }
     }
-  
+
     setTimeout(() => {
       if (!isAutoConnectRunning) return;
-      
+
       let thereAreConnectButtonsLeft = false;
-  
-      if (document.querySelectorAll(linkedInSelector.connectButtonsFromSearchPage).length > 0) {
+
+      if (
+        document.querySelectorAll(linkedInSelector.connectButtonsFromSearchPage)
+          .length > 0
+      ) {
         thereAreConnectButtonsLeft = true;
       }
-  
+
       if (thereAreConnectButtonsLeft) {
         addPeopleFromSearchPage();
       } else {
         goToNextPage();
       }
     }, alreadyInvited * delayBetweenClicks + 1000);
-  };
-  
+  }
+
   function addPeopleFromRecommendedForYouPage() {
     if (!isAutoConnectRunning) return;
-  
+
     const delayBetweenClicks = 2000;
     let alreadyInvited = 0;
-  
-    const connectButtons = document.querySelectorAll(linkedInSelector.connectButtonsFromRecommendedPage);
-  
+
+    const connectButtons = Array.from(
+      document.querySelectorAll(
+        linkedInSelector.connectButtonsFromRecommendedPage
+      )
+    )
+      .filter(
+        (element) =>
+          element.innerText === "Connect" &&
+          typeof element.parentElement.click !== "undefined"
+      )
+      .map((element) => element.parentElement);
+
     window.scrollTo(0, document.body.scrollHeight);
-  
+
     if (connectButtons.length > 0) {
       for (const item of connectButtons) {
         setTimeout(() => {
@@ -100,25 +117,26 @@
         }, alreadyInvited++ * delayBetweenClicks);
       }
     }
-  
+
     setTimeout(() => {
       addPeopleFromRecommendedForYouPage();
     }, alreadyInvited * delayBetweenClicks + 1000);
-  };
-  
-  function isOnSearchPage () {
+  }
+
+  function isOnSearchPage() {
     return location.href.includes(linkedInUrl.patternOfSearchPage);
   }
-  
-  function isOnRecommendedForYouPage () {
+
+  function isOnRecommendedForYouPage() {
     return location.href.includes(linkedInUrl.patternOfMyNetworkPage);
   }
-  
+
   function checkIfUrlHasChanged() {
-    if (!isAutoConnectRunning || location.href === autoConnectLastLocation) return;
-  
+    if (!isAutoConnectRunning || location.href === autoConnectLastLocation)
+      return;
+
     autoConnectLastLocation = location.href;
-  
+
     if (isOnSearchPage()) {
       addPeopleFromSearchPage();
     } else if (isOnRecommendedForYouPage()) {
@@ -126,25 +144,25 @@
     } else {
       isAutoConnectRunning = false;
     }
-  };
+  }
 
   setInterval(checkIfUrlHasChanged, 1000);
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
     switch (message) {
-      case 'isAutoConnectAvailable':
+      case "isAutoConnectAvailable":
         sendResponse(true);
         break;
-      case 'isAutoConnectRunning':
+      case "isAutoConnectRunning":
         sendResponse(isAutoConnectRunning);
         break;
-      case 'startAutoConnect':
+      case "startAutoConnect":
         isAutoConnectRunning = true;
         sendResponse(isAutoConnectRunning);
         break;
-      case 'stopAutoConnect':
+      case "stopAutoConnect":
         isAutoConnectRunning = false;
-        autoConnectLastLocation = ""
+        autoConnectLastLocation = "";
         sendResponse(isAutoConnectRunning);
         break;
     }
