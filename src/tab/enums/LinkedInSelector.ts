dee665421d7c45cdbfb814796184fb011f5ae1fb
Victor Nogueira <felladrin@gmail.com>
2021-02-28 15:54:49 +0200

    Update CSS Selectors
    Resolves #15

--- src/tab/enums/LinkedInSelector.ts
+++ src/tab/enums/LinkedInSelector.ts
@@ -2,6 +2,6 @@ export enum LinkedInSelector {
   SendNowButton = "div.send-invite > div.artdeco-modal__actionbar > button.artdeco-button--primary:enabled",
   CancelButton = "div.send-invite > button.artdeco-modal__dismiss",
   NextButton = "button.artdeco-pagination__button--next",
-  ConnectButtonsFromRecommendedPage = "div.discover-entity-type-card__bottom-container > footer > button:enabled",
-  ConnectButtonsFromSearchPage = "button.search-result__action-button.search-result__actions--primary:enabled",
+  ConnectButtonsFromRecommendedPage = "div.discover-entity-type-card__bottom-container button.ember-view:enabled",
+  ConnectButtonsFromSearchPage = "li.reusable-search__result-container button.ember-view:enabled:not([data-control-id])",
 }
