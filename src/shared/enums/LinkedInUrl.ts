f94c8f3add3049fad51e5cb68ef7f2d12ec879a8
Victor Nogueira <felladrin@gmail.com>
2021-05-30 00:51:10 +0300

    Update SearchPeoplePage URL, as the previous one was not working properly

--- src/shared/enums/LinkedInUrl.ts
+++ src/shared/enums/LinkedInUrl.ts
@@ -1,5 +1,5 @@
 export enum LinkedInUrl {
-  SearchPeoplePage = "https://www.linkedin.com/search/results/people/?network=%5B%22S%22%5D",
+  SearchPeoplePage = "https://www.linkedin.com/search/results/people/",
   MyNetworkPage = "https://www.linkedin.com/mynetwork/",
   PatternOfSearchPage = "linkedin.com/search/results/people",
   PatternOfMyNetworkPage = "linkedin.com/mynetwork",
