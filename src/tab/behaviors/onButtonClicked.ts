01535c81d25a11063145b5f0bee4ec6b83352161
Victor Nogueira <felladrin@gmail.com>
2021-06-01 12:44:21 +0300

    Move "delay" to shared functions

--- src/tab/behaviors/onButtonClicked.ts
+++ src/tab/behaviors/onButtonClicked.ts
@@ -1,7 +1,7 @@
 import randomInt from "random-int";
 import { buttonClickRequested } from "../events/buttonClickRequested";
 import { buttonClicked } from "../events/buttonClicked";
-import { delay } from "../functions/delay";
+import { delay } from "../../shared/functions/delay";
 
 buttonClicked.watch(async () => {
   await delay(randomInt(1500, 3000));
