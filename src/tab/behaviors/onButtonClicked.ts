d3034b36671ab830e1b0eaee291ce7eecaa08841
Victor Nogueira <felladrin@gmail.com>
2021-06-13 01:13:08 +0300

    Automatically stop auto-connecting if number of button clicks is equal or greater than the limit set

--- src/tab/behaviors/onButtonClicked.ts
+++ src/tab/behaviors/onButtonClicked.ts
@@ -1,8 +1,11 @@
 import randomInt from "random-int";
 import { buttonClicked } from "../events/buttonClicked";
 import { dismissSendInviteDialog } from "../effects/dismissSendInviteDialog";
-import { sample } from "effector";
+import { combine, guard, sample } from "effector";
 import { delayNextClick } from "../effects/delayNextClick";
+import { stopped } from "../events/stopped";
+import { buttonClicksCountStore } from "../stores/buttonClicksCountStore";
+import { maximumAutoConnectionsPerSessionStore } from "../../shared/stores/maximumAutoConnectionsPerSessionStore";
 
 sample({ clock: buttonClicked, target: dismissSendInviteDialog });
 
@@ -10,4 +13,15 @@ sample({
   clock: buttonClicked,
   fn: () => randomInt(1500, 3000),
   target: delayNextClick,
+});
+
+guard({
+  clock: buttonClicked,
+  source: combine({
+    buttonClicksCount: buttonClicksCountStore,
+    maximumAutoConnectionsPerSession: maximumAutoConnectionsPerSessionStore,
+  }),
+  filter: ({ buttonClicksCount, maximumAutoConnectionsPerSession }) =>
+    buttonClicksCount >= Number(maximumAutoConnectionsPerSession),
+  target: stopped,
 });
