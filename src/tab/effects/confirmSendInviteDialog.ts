48c38e76d73cfcc49ca0263e19854c474dd5ccf1
Victor Nogueira <felladrin@gmail.com>
2022-09-17 02:20:49 +0300

    Re-try a few times if an element is not found on the page right away

--- src/tab/effects/confirmSendInviteDialog.ts
+++ src/tab/effects/confirmSendInviteDialog.ts
@@ -1,6 +1,20 @@
 import { createEffect } from "effector";
 import { LinkedInCssSelector } from "../../shared/enums/LinkedInCssSelector";
 
-export const confirmSendInviteDialog = createEffect(() => {
-  document.querySelector<HTMLButtonElement>(LinkedInCssSelector.SendButtonFromSendInviteModal)?.click();
-});
+export const confirmSendInviteDialog = createEffect(
+  () =>
+    new Promise((resolve) => {
+      let attempts = 0;
+
+      const interval = setInterval(() => {
+        const sendButton = document.querySelector<HTMLButtonElement>(LinkedInCssSelector.SendButtonFromSendInviteModal);
+
+        sendButton?.click();
+
+        if (sendButton || ++attempts > 5) {
+          clearInterval(interval);
+          resolve(null);
+        }
+      }, 500);
+    })
+);
