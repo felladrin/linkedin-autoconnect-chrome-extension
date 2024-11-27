c36d10e9eda645c5db9da2de046eda55cf472086
Victor Nogueira <felladrin@gmail.com>
2024-11-27 18:44:49 +0100

    Update CSS selector for buttons from Search Page
    Fixes #657
    Fixes #634

--- src/shared.ts
+++ src/shared.ts
@@ -20,7 +20,7 @@ export interface ChromePortMessage {
 export enum LinkedInCssSelector {
   NextPageButton = "button.artdeco-pagination__button--next",
   ConnectButtonFromMyNetworkPage = "div.discover-entity-type-card__bottom-container button.ember-view:enabled:not(.artdeco-button--muted):not(.artdeco-button--full)",
-  ConnectButtonFromSearchPage = "div.search-results-container div.entity-result__actions > div > button.ember-view:enabled:not(.artdeco-button--muted)",
+  ConnectButtonFromSearchPage = "div.search-results-container div > button.ember-view:enabled:not(.artdeco-button--muted)",
   SendButtonFromSendInviteModal = "div.send-invite button.artdeco-button--primary",
 }
 
